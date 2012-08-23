4e1beabe89c5b993f6c505df0586650a0420757e
Kyle Zarazan <kyle@brocktoncg.com>
2012-08-23 09:47:48 -0500

    0.7 release

--- lib/generators/gemboree/install_generator.rb
+++ lib/generators/gemboree/install_generator.rb
@@ -3,21 +3,23 @@ module Gemboree
     class InstallGenerator < Rails::Generators::Base
     
       def setup_home_page
-        create_file "app/controllers/home_controller.rb" do
-          "class HomeController < ApplicationController
-            def index
-            end
-          end"
-        end
+        create_file "app/controllers/home_controller.rb", "class HomeController < ApplicationController\n  def index\n  end\nend"
         empty_directory "app/views/home/"
         create_file "app/views/home/index.html.erb", "<h1>Gemboree Initiated</h1>"
-        remove_file 'app/public/index.html'
+        remove_file 'public/index.html'
+        remove_file 'app/assets/images/rails.png'
         route "root :to => 'home#index'"
       end
       
       def setup_asssets
-        insert_into_file "app/assets/javascripts/application.js", "//= require gemboree", :after => "//= require jquery_ujs"
-        insert_into_file "app/assets/stylesheets/application.css", " *= require gemboree", :after => " *= require_self"
+        insert_into_file "app/assets/javascripts/application.js", "\n //= require gemboree", :after => "//= require jquery_ujs"
+        insert_into_file "app/assets/stylesheets/application.css", "\n  *= require gemboree", :after => " *= require_self"
+      end
+    
+      def setup_cancan
+        generate "cancan:ability"
+        insert_into_file "app/models/ability.rb", "\n  can :manage, :all", :after => "def initialize(user)"
+        insert_into_file "app/controllers/application_controller.rb", "\n  def current_user\n    nil\n  end", :after => "protect_from_forgery"
       end
     
     end
