bb3f6f62aba2112522c9c48cb0c95a13c1be58a0
Kyle Zarazan <kyle@brocktoncg.com>
2012-09-07 10:00:22 -0500

    change bootstrap javascript load order

--- app/assets/javascripts/gemboree.js
+++ app/assets/javascripts/gemboree.js
@@ -1,1 +1,13 @@
+//= require bootstrap-transition.js
+//= require bootstrap-alert.js
+//= require bootstrap-button.js
+//= require bootstrap-carousel.js
+//= require bootstrap-collapse.js
+//= require bootstrap-dropdown.js
+//= require bootstrap-modal.js
+//= require bootstrap-tooltip.js
+//= require bootstrap-popover.js
+//= require bootstrap-scrollspy.js
+//= require bootstrap-tab.js
+//= require bootstrap-typeahead.js
 //= require_tree .
