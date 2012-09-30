a74b9ed722b1d50f377b4a5321e3655bc5955dd4
Kyle Zarazan <kyle@brocktoncg.com>
2012-09-30 14:50:10 -0500

    comment out automatic button loading function

--- app/assets/javascripts/scaffolds.js
+++ app/assets/javascripts/scaffolds.js
@@ -2,15 +2,17 @@ $(function() {
 
 //Button loading indicator
 $('.btn').attr("data-loading-text", "loading...");
-$('.btn').live('click', function(e) { $(this).button('loading') });
+//$('.btn').live('click', function(e) { $(this).button('loading') });
 
 //Change edit button to yellow on mouseover
 $(".btn-edit").hover(function(e) { $(this).addClass('btn-warning'); },
 function(e) { $(this).removeClass('btn-warning'); });
 
 //Change destroy button to red on mouseover
-$(".btn-destroy").hover(function(e) { $(this).addClass('btn-danger'); },
-function(e) { $(this).removeClass('btn-danger'); });
+$(".btn-destroy").hover(function(e) { 
+	$(this).addClass('btn-danger'); },
+	function(e) { $(this).removeClass('btn-danger'); 
+});
 
 //Add sort direction indicators
 $('.current.asc').before('<i class="icon-arrow-up">');
