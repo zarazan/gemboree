b27285a5671ab6d72cced93e5d1e6e33e02cc841
Kyle Zarazan <kyle@brocktoncg.com>
2012-10-09 10:43:30 -0500

    added acts_as_actor model validations

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,22 @@
 class Role < ActiveRecord::Base
   
+	before_validation :default_access
+
   attr_accessible :name, :access_level
   
   has_many :user_roles, :dependent => :destroy
   has_many :users, :through => :user_roles
   
+  validates :name, 
+  	:presence => true,
+  	:uniqueness => true
+
+  validates :access_level, :presence => true
+
+private
+
+  def default_access
+  	self.access_level ||= 0
+  end
+
 end
--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,7 +1,11 @@
 class UserRole < ActiveRecord::Base
   
   attr_accessible :role_id, :user_id
+
   belongs_to :role
   belongs_to :user
   
+  validates :role_id, :presence => true
+  validates :user_id, :presence => true
+
 end
