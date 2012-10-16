478991d421fc1d2dbd165fa00e4325453197c5ef
Kyle Zarazan <kyle@brocktoncg.com>
2012-10-16 13:05:43 -0500

    remove space in scaffold controller

--- lib/templates/rails/scaffold_controller/controller.rb
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -3,9 +3,8 @@ require_dependency "<%= namespaced_file_path %>/application_controller"
 
 <% end -%>
 <% module_namespacing do -%>
-
 class <%= controller_class_name %>Controller < ApplicationController
-  
+
   load_and_authorize_resource
   helper_method :sort_column, :sort_direction
   
